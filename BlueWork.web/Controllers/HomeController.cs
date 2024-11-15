dcdedad2ffbe96d16e78288b0af93e82d06b58bb
Rextine Evangelista <rextinevangelista@gmail.com>
2024-11-15 23:48:52 +0800

    change

--- BlueWork.web/Controllers/HomeController.cs
+++ BlueWork.web/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace BlueWork.web.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Home()
         {
             return View();
         }
