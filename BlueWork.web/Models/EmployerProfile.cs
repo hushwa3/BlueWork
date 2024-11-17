408d9664d9a694afac935ab9e6da153d5e23aa84
Rextine Evangelista <rextinevangelista@gmail.com>
2024-11-17 17:22:11 +0800

    change

--- BlueWork.web/Models/EmployerProfile.cs
+++ BlueWork.web/Models/EmployerProfile.cs
@@ -7,6 +7,11 @@ namespace BlueWork.web.Models
     {
         [Key]
         public int EmployerID { get; set; }
+        public string FirstName { get; set; }  
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int ContactNumber { get; set; }
+        public DateOnly DateOfBirth { get; set; }
         public string CompanyName { get; set; }
         public string CompanyDescription { get; set; }
         public string Industry { get; set; }
