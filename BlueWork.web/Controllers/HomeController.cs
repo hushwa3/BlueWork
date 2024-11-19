751f4920b6c5a603f3faff4f38883f76d19b7133
Rextine Evangelista <rextinevangelista@gmail.com>
2024-11-19 18:31:33 +0800

    change

--- BlueWork.web/Controllers/HomeController.cs
+++ BlueWork.web/Controllers/HomeController.cs
@@ -47,6 +47,10 @@ namespace BlueWork.web.Controllers
         {
             return View();
         }
+        public IActionResult JobsPost()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
